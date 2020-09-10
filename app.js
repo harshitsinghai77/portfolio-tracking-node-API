3848121749cf577207f28976ba2f7e174f7ab212
harshitsinghai77 <harshitsinghai77@gmail.com>
2020-09-10 21:43:35 +0530

    Removed commented code

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+
 const serverResponseHandler = require("./middleware/serverResponseHandler");
 const contentTypeHandler = require("./middleware/contentTypeHandler");
-
 const Trades = require("./routes/trade/trade");
 const Portfolio = require("./routes/portfolio/portfolio");
 require("dotenv").config();
@@ -26,10 +26,6 @@ mongoose
   .then(() => console.log("Conncected to database"))
   .catch((err) => console.log(err));
 
-// mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0-uegxx.mongodb.net/test?retryWrites=true&w=majority`, {dbName:'WeBase',useNewUrlParser: true})
-//     .then(res => console.log('Connected to datatbase'))
-//     .catch(err => console.log(err))
-
 //middlewares
 app.use(morgan("dev"));
 app.use(cors());
@@ -43,10 +39,6 @@ app.use(serverResponseHandler());
 //Routes
 app.use("/trade", Trades);
 app.use("/portfolio", Portfolio);
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
